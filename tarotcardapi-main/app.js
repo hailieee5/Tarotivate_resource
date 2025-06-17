2356a729dc310911035e5447e14cf193c40c511a
hailieee5 <hailiehhc@gmail.com>
2025-06-17 10:40:02 +0800

    Update app.js

--- tarotcardapi-main/app.js
+++ tarotcardapi-main/app.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const cardRoutes = require("./routes/cardRoutes"); // Import modular route handlers
-const errorHandler = require("./middlewares/errorHandler"); // Import error handling middleware
-const ieltsRoutes = require("./routes/ieltsRoutes"); // 新增雅思路由
+const cardRoutes = require("./routes/cardRoutes");
+const errorHandler = require("./middlewares/errorHandler");
+const ieltsRoutes = require("./routes/ieltsRoutes");
 const path = require('path');
 
 dotenv.config();
 const app = express();
-const port = process.env.PORT || 3000; // Use a default port if PORT environment variable is not defined
+const port = process.env.PORT || 3000; // Use Vercel-assigned port or default to 3000
 
 // Health check route
 app.get('/', (req, res) => {
@@ -19,14 +19,9 @@ app.use(express.json());
 app.use("/tarotdeck", express.static(path.join(__dirname, 'images')));
 app.use('/audio', express.static(path.join(__dirname, 'IELTS_100_Sentence-main')));
 
-// Mount the cardRoutes router
 app.use("/cards", cardRoutes);
-
-// Mount the ieltsRoutes router under /api
 app.use("/api", ieltsRoutes);
 
-
-// Handle errors using the errorHandler middleware
 app.use(errorHandler);
 
 app.listen(port, () => {
